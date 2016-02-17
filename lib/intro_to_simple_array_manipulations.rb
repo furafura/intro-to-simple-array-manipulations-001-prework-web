6f50f9b1c0adba084ee6f1f96edcdb43956ce455
kazushige Miyao <yura.soverigns@gmail.com>
2016-02-17 17:30:48 +0900

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,56 @@
+def using_push(array, char)
+	array.push(add)
+end
+
+def using_unshift(array, char)
+	array.unshift(char)
+end
+
+def using_push(array, char)
+	array.push(char)
+end
+
+def using_pop(array)
+	array.pop
+end
+
+def pop_with_args(array)
+	array.pop(2)
+end
+
+def using_shift(array)
+	array.shift
+end
+
+def shift_with_args(array)
+	array.shift(2)
+end
+
+def using_concat(array, new_array)
+	array.concat(new_array)
+end
+
+def using_insert(array, value)
+	array.insert(4,value)
+end
+
+def using_uniq(array)
+	array.uniq
+end
+
+def using_flatten(array)
+	array.flatten
+end
+
+def using_delete(array, value)
+	array.delete(value)
+end
+
+def using_delete_at(array, index)
+	array.delete_at(index)
+end
+
+
+
+
+
